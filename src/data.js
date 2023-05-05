ff3b1fa553fc81361db3bf0b06c7b8a9c182562d
Fitria Intani <nanditama.intani99@gmail.com>
2023-05-05 10:43:29 +0700

    inital commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: "Pizza Pepperoni",
+            slug: "pizza-pepperoni",
+            category: "Food",
+            image: "/images/pizzapepperoni.jpg",
+            price: 75000,
+            countInStocke: 10,
+            brand: "FoodYum",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Pizza with pepperoni and Mozarella"
+        },
+        {
+            name: "Spaghetti Bolognese",
+            slug: "spaghetti-bolognese",
+            category: "Food",
+            image: "/images/spaghettibolognese.jpg",
+            price: 45000,
+            countInStocke: 10,
+            brand: "FoodYum",
+            rating: 4,
+            numReviews: 10,
+            description: "Spaghetti with bolognese sauce"
+        },
+        {
+            name: "Espresso",
+            slug: "espresso-coffee",
+            category: "Beverages",
+            image: "/images/espresso.jpg",
+            price: 20000,
+            countInStocke: 10,
+            brand: "FoodYum",
+            rating: 4,
+            numReviews: 10,
+            description: "Espresso coffee from Italy"
+        },
+        {
+            name: "Jasmine Tea",
+            slug: "jasmine-tea",
+            category: "Beverages",
+            image: "/images/jasminetea.jpg",
+            price: 15000,
+            countInStocke: 10,
+            brand: "FoodYum",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Jasmine tea from Indonesia"
+        },
+    ],
+};
+
+export default data
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,149 @@
+import axios from "axios";
+import React, { useContext, useEffect, useReducer } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useNavigate, useParams } from "react-router-dom";
+import Rating from "../components/Rating";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
+import { Store } from "../Store";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, product: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function ProductScreen() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const { slug } = params;
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStocke < quantity) {
+      window.alert("Maaf, produk tidak tersedia");
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity },
+    });
+    navigate("/cart");
+  };
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error} </MessageBox>
+  ) : (
+    <div>
+      <Row>
+        <Col md={6}>
+          <img
+            className="img-large"
+            alt={product.name}
+            src={product.image}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>Rp{product.price}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description:
+              <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroupItem>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>Rp{product.price}</Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Status</Col>
+                    <Col>
+                      {product.countInStocke > 0 ? (
+                        <Badge bg="success">Tersedia</Badge>
+                      ) : (
+                        <Badge bg="danger">Tidak tersedia</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  {product.countInStocke > 0 && (
+                    <div className="d-grid">
+                      <Button onClick={addToCartHandler} variant="primary">
+                        Add to cart
+                      </Button>
+                    </div>
+                  )}
+                </ListGroupItem>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
